feab44f114dd1bc65b9e011b1b9616ebd574fe17
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-10-25 22:25:20 -0700

    add components outline

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,7 @@
+import './intro.scss';
+
+const intro = () => {
+  return <div className='intro'></div>;
+};
+
+export default intro;
---
+++ src/components/topbar/topbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './topbar.scss';
+
+const topbar = () => {
+  return <div className='.topbar'></div>;
+};
+
+export default topbar;
